8e16ea895ba8a4b3d05259a922fec6749c158802
Issac Li <issac.li.523@gmail.com>
2024-05-14 12:54:08 +0800

    refactor: flatten code structure

--- src/packages/Prettier/prettier/insertScripts/index.ts
+++ src/packages/Prettier/prettier/insertScripts/index.ts
@@ -4,30 +4,32 @@ import { addScriptToPackageJson, managerRun } from '../../../../utilities/cli';
 const insertScripts = () => {
   const { env, prettier, styleSheet, typescript, withSyntaxExtension } = getAnswers();
 
-  if (prettier) {
-    const { addIgnores, createConfig } = prettier;
+  if (!prettier) return;
 
-    const config = createConfig ? ' --config .prettier.config.js' : '';
-    const ignore = addIgnores ? ' --ignore-path .prettierignore' : '';
+  const { addIgnores, createConfig } = prettier;
 
-    const extensions = [
-      styleSheet !== 'none' && (styleSheet === 'scss' ? '?(s)css' : 'css'),
-      env.isBrowser && ['html'],
-      withSyntaxExtension ? 'js' : 'js?(x)',
-      !!typescript && (withSyntaxExtension ? 'ts' : 'ts?(x)'),
-      'json'
-    ]
-      .flat()
-      .filter(Boolean)
-      .join(',');
+  const config = createConfig ? ' --config .prettier.config.js' : '';
+  const ignore = addIgnores ? ' --ignore-path .prettierignore' : '';
 
-    addScriptToPackageJson([
-      ['// Prettier', '---------- ---------- ---------- ---------- ----------'],
-      ['prettier', managerRun('prettier:base -c')],
-      ['prettier:base', `prettier './**/*.{${extensions}}' --cache${config}${ignore}`],
-      ['prettier:fix', managerRun('prettier:base -w')]
-    ]);
-  }
+  const extensions = [
+    styleSheet !== 'none' && (styleSheet === 'scss' ? '?(s)css' : 'css'),
+    env.isBrowser && ['html'],
+    withSyntaxExtension ? 'js' : 'js?(x)',
+    !!typescript && (withSyntaxExtension ? 'ts' : 'ts?(x)'),
+    'json'
+  ]
+    .flat()
+    .filter(Boolean)
+    .join(',');
+
+  addScriptToPackageJson([
+    ['// Prettier', '---------- ---------- ---------- ---------- ----------'],
+    ['prettier', managerRun('prettier:base -c')],
+    ['prettier:base', `prettier './**/*.{${extensions}}' --cache${config}${ignore}`],
+    ['prettier:fix', managerRun('prettier:base -w')]
+  ]);
+
+  // TODO: Add prettier & lint script
 };
 
 export default insertScripts;
