b7408cd3b981c9797cef814d2c419694ded82ec5
Issac Li <issac.li.523@gmail.com>
2024-05-17 19:05:22 +0800

    fix: fixed invalid import

--- src/packages/Prettier/prettier/createConfig/index.ts
+++ src/packages/Prettier/prettier/createConfig/index.ts
@@ -1,7 +1,7 @@
 import { writeBeforeWrite } from '../../../../utilities/fs';
 import { PWD } from '../../../../utilities/fs/constants';
 
-import createPrettierConfig from './template.js';
+import createPrettierConfig from './template';
 
 const createConfig = () => {
   writeBeforeWrite(PWD('./.prettier.config.js'), createPrettierConfig());
