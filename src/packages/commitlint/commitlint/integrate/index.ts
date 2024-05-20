7b3e8848ab4d754b1670786afc2cedff51f3bef1
Issac Li <issac.li.523@gmail.com>
2024-05-20 17:34:08 +0800

    fix: add check when integrate with husky

--- src/packages/commitlint/commitlint/integrate/index.ts
+++ src/packages/commitlint/commitlint/integrate/index.ts
@@ -1,9 +1,14 @@
+import { getAnswers } from '../../../../store/answers.store';
 import { writeBeforeWrite } from '../../../../utilities/fs';
 import { PWD } from '../../../../utilities/fs/constants';
 
 import createCommitMsgHook from './template';
 
 const integrate = () => {
+  const { husky } = getAnswers();
+
+  if (!husky) return;
+
   writeBeforeWrite(PWD('./.husky/commit-msg'), createCommitMsgHook());
 };
 
