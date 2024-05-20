6eca5ed635e63c875342ae3a57021021d50b6c40
Issac Li <issac.li.523@gmail.com>
2024-05-20 17:28:20 +0800

    fix(commitlint): fixed incorrect function name

--- src/packages/commitlint/commitlint/integrate/index.ts
+++ src/packages/commitlint/commitlint/integrate/index.ts
@@ -1,10 +1,10 @@
 import { writeBeforeWrite } from '../../../../utilities/fs';
 import { PWD } from '../../../../utilities/fs/constants';
 
-import createPreCommitHook from './template';
+import createCommitMsgHook from './template';
 
 const integrate = () => {
-  writeBeforeWrite(PWD('./.husky/commit-msg'), createPreCommitHook());
+  writeBeforeWrite(PWD('./.husky/commit-msg'), createCommitMsgHook());
 };
 
 export default integrate;
--- src/packages/commitlint/commitlint/integrate/template.ts
+++ src/packages/commitlint/commitlint/integrate/template.ts
@@ -1,15 +1,15 @@
 import { getAnswers } from '../../../../store/answers.store';
 import { PACKAGE_MANAGERS } from '../../../../utilities/constants';
 
-const createPreCommitHook = () => {
+const createCommitMsgHook = () => {
   return [
     '#!/usr/bin/env sh',
     '',
     '. "$(dirname -- "$0")/_/husky.sh"',
     '. "$(dirname -- "$0")/common.sh"',
     '',
-    `${PACKAGE_MANAGERS[getAnswers().packageManager].scripts.commitLint} commitlint --edit $1`
+    `${PACKAGE_MANAGERS[getAnswers().packageManager].scripts.execute} commitlint --edit $1`
   ].join('\n');
 };
 
-export default createPreCommitHook;
+export default createCommitMsgHook;
