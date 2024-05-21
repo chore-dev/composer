2a338fd193f56a30db5f12140217a44a8406f574
Issac Li <issac.li.523@gmail.com>
2024-05-21 17:59:17 +0800

    fix(Husky): fixed husky directory unable to init issue

--- src/packages/Husky/husky/insertScripts/index.ts
+++ src/packages/Husky/husky/insertScripts/index.ts
@@ -1,10 +1,10 @@
-import { addScriptToPackageJson, managerRun } from '../../../../utilities/cli';
+import { addScriptToPackageJson, execInPwd, managerRun } from '../../../../utilities/cli';
 import { backupBeforeCopy } from '../../../../utilities/fs';
 import { PWD, templateDirectory } from '../../../../utilities/fs/constants';
 
 const insertScripts = () => {
   addScriptToPackageJson([['prepare', 'husky']]);
-  managerRun('prepare');
+  execInPwd(managerRun('prepare'));
   backupBeforeCopy(templateDirectory('common.sh'), PWD('./.husky/common.sh'));
 };
 
