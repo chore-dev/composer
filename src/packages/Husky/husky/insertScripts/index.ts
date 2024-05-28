16eaac1873d829c62e558e7736a7696e1fda7fc7
Issac Li <issac.li.523@gmail.com>
2024-05-28 19:03:53 +0800

    fix: fixed composer crash if user didn't init git when preparing husky

--- src/packages/Husky/husky/insertScripts/index.ts
+++ src/packages/Husky/husky/insertScripts/index.ts
@@ -1,11 +1,20 @@
 import { addScriptToPackageJson, execInPwd, managerRun } from '../../../../utilities/cli';
-import { backupBeforeCopy } from '../../../../utilities/fs';
+import { backupBeforeCopy, isPathExist } from '../../../../utilities/fs';
 import { PWD, templateDirectory } from '../../../../utilities/fs/constants';
+import { error } from '../../../../utilities/logger';
 
 const insertScripts = () => {
   addScriptToPackageJson([['prepare', 'husky']]);
-  execInPwd(managerRun('prepare'));
-  backupBeforeCopy(templateDirectory('common.sh'), PWD('./.husky/common.sh'));
+
+  if (isPathExist(PWD('./.git'))) {
+    execInPwd(managerRun('prepare'));
+    backupBeforeCopy(templateDirectory('common.sh'), PWD('./.husky/common.sh'));
+  } else {
+    error(
+      [true, true],
+      'Failed to initialize husky. Git is not initialized in this directory, run `git init` first.'
+    );
+  }
 };
 
 export default insertScripts;
