ae4e166e2f272f06c93aa88ac2c20e7f966b28d6
Issac Li <issac.li.523@gmail.com>
2024-05-31 18:16:12 +0800

    fix(commitlint): relocate commitlint dependencies to devDependencies

--- src/packages/commitlint/commitlint/install/index.ts
+++ src/packages/commitlint/commitlint/install/index.ts
@@ -1,7 +1,7 @@
 import { managerInstall } from '../../../../utilities/cli';
 
 const install = () => {
-  managerInstall('@commitlint/cli @commitlint/config-conventional');
+  managerInstall('@commitlint/cli @commitlint/config-conventional', true);
 };
 
 export default install;
